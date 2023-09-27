7333351fe6dcccccf7e2267dcb54c256319520d5
Williamhsf <williamhenriquefb@gmail.com>
2023-09-26 21:12:09 -0300

    header structure

--- mary-cakes/assets/js/main.js
+++ mary-cakes/assets/js/main.js
@@ -1,1 +1,9 @@
-body
+const menu = document.querySelector(".main-menu");
+const hamburguer = document.querySelector(".btn-hamburguer");
+
+function toggleMenu() {
+  menu.classList.toggle("--is-visible");
+  hamburguer.classList.toggle("--is-active");
+}
+
+hamburguer.addEventListener("click", toggleMenu);
